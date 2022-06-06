c0963d11b34d88cc47fe3ab08e59454815a76b2f
thalerngsak <oodramcom@gmail.com>
2022-06-06 19:31:08 +0700

    Add Spring to project

---
+++ src/test/kotlin/com/thalerngsak/kotlin/bitcoin/BitcoinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.thalerngsak.kotlin.bitcoin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BitcoinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
